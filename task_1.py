d39598fbf6d2be26c138008637f1ef017af3fc51
aryaanraaj <164717063+aryaanraaj@users.noreply.github.com>
2025-07-10 11:10:52 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,12 @@
+#Task 1: Perform Basic Mathematical Operations
+
+a=int(input("Enter your first number: "))
+b=int(input("Enter your second number: "))
+sum = a+b
+sub = a-b
+div = a/b
+mul = a*b
+print("Addition: ", sum)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division", div)
---
+++ task_2.py
@@ -0,0 +1,5 @@
+fn = input("Enter your first name: ")
+ln = input("Enter your last name: ")
+c = fn+ ln
+print(c)
+print("Hello,",c,"! welcome to python program")
